6f22f451e2719ec0294ee700c0909d7780742acc
Jesssullivan <jess@sulliwood.org>
2019-02-15 11:28:20 -0500

    + Update subprocess.run() with args

--- StressTesting-with-Chapel/Timer.py
+++ StressTesting-with-Chapel/Timer.py
@@ -1,26 +1,30 @@
-''''
-Python3 method to evaluate script times.
-WIP by Jess Sullivan.
-''''
 import subprocess
 import time
 
 File = "./FileCheck"
-opt = " --S"
+opt = "--S"
+
 loopNum = 10
 
 runTime1 = []
 runTime2 = []
 
-def iterateScript(loops, F, A, runTime):
+def iterateScript(loops, F, runTime):
     for l in range(loops):
         start = time.time()
-        subprocess.run([F,A])
+        subprocess.run([F])
         end = time.time()
         runTime.append(end-start)
 
-iterateScript(loopNum, File, "", runTime1)
-iterateScript(loopNum, File, "", runTime2)
+def iterateWithArgs(loops, F, arg, runTime):
+    for l in range(loops):
+        start = time.time()
+        subprocess.run([F, arg])
+        end = time.time()
+        runTime.append(end-start)
 
-print("average runTime for no args is ", sum(runTime1) / loops)
-print("average runTime with args is ", sum(runTime2) / loops)
+iterateScript(loops=loopNum, F=File, runTime=runTime1)
+iterateWithArgs(loops=loopNum, F=File, arg=str(opt), runTime=runTime2)
+
+print("average runTime for no args is ", sum(runTime1) / loopNum)
+print("average runTime with args is ", sum(runTime2) / loopNum)
